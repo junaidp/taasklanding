20cc1cfcb194a432275678cc7f620d28a3b33922
Munna <104292892+Sardar-Murad-Ali@users.noreply.github.com>
2023-07-29 14:48:51 +0500

    Delete src directory

--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import Home from "./Home";
-import About from "./About";
-import Pricing from "./Pricing";
-import Testimonial from "./Testimonial";
-import Blog from "./Blog";
-import Product from "./Product";
-
-export { Home, About, Pricing, Testimonial, Blog, Product };
--- src/store/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import appSlice from "../features/appSlice";
-
-export const store = configureStore({
-  reducer: {
-    store: appSlice,
-  },
-});
